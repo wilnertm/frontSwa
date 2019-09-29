2f13529bf869d4ceab1d0b83b280915118bdb058
Pabl10 <35938565+Pabl10@users.noreply.github.com>
2019-09-29 15:26:41 -0500

    maquetacion html, css y logica de componente para peliculas

--- src/app/peliculas/peliculas.component.ts
+++ src/app/peliculas/peliculas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GeneralService } from '../general.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-peliculas',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeliculasComponent implements OnInit {
 
-  constructor() { }
+  public peliculas: any[] = []
+  public busqueda: string = "";
+  public firstCharge: boolean = false;
+  constructor(
+    private general: GeneralService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.general.getRequest({}, "/films/").then(data => {
+      this.peliculas = data['results'];
+      this.firstCharge = true;
+      this.peliculas = this.filterBy('episode_id');
+    })
   }
+  filterBy(prop: string) {
+    console.log("BUSQUEDA__", prop);
+    prop = "title = " + +prop
+    if (prop.length === 0) { this.peliculas = this.filterBy('episode_id'); }
+    return this.peliculas.sort((a, b) => a[prop] > b[prop] ? 1 : a[prop] === b[prop] ? 0 : -1);
+  }
+
+  filterByName(prop: string) {
+    if (prop.length === 0) { this.peliculas = this.filterBy('episode_id'); }
+    return this.peliculas.sort((a, b) => a['title'] > b['title'] ? 1 : a['title'] === b['title'] ? 0 : -1);    
+  }
+
+movieSelect(movie) {
+  let url_new = movie['url']
+  let initial = url_new.length - 2
+  var direction = url_new.substring(initial);
+  this.router.navigate([`/peliculas-detail/${direction}`], { relativeTo: direction, });
+}
+
+filterItems(query) {
+  return this.peliculas = this.peliculas.find(this.peliculas['title'] = query);
+}
 
 }
